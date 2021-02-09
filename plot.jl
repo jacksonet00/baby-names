7047d37d0df7b6a7a0b5f70adc065e238cb938ae
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-02-08 23:59:15 -0500

    style plot

--- plot.jl
+++ plot.jl
@@ -2,25 +2,50 @@ using SQLite
 using Gadfly
 using DataFrames
 
-# TODO: Add normalized frequency
-
 function plot(db_name, name, sex)
    db = SQLite.DB(db_name)
 
    query = """
-   SELECT year AS Year, num AS Frequency
-   FROM names
-   WHERE lower(name)=lower("$(name)") and lower(sex)=lower("$(sex)")
-   ORDER BY year;
+   SELECT name, n.year as Year, num as Count, (num * 1.0 / pop) * 100 as Normalized
+   FROM (
+      SELECT year, sum(num) as pop
+      FROM names
+      GROUP BY year
+   ) t1, names n
+   WHERE t1.year = n.year AND LOWER(name)=LOWER("$(name)") AND LOWER(sex)=LOWER("$(sex)");
    """
 
    data = DBInterface.execute(db, query) |> DataFrame
 
-   plt = Gadfly.plot(data, x=:Year, y=:Frequency, Geom.bar, Theme(panel_fill=colorant"black", default_color=colorant"orange"))
-   normalized_plt = Gadfly.plot(data, x=:Year, y=:Frequency, Geom.bar, Theme(panel_fill=colorant"black", default_color=colorant"orange"))
+   Gadfly.push_theme(:dark)
+
+   plt = Gadfly.plot(
+      data,
+      x=:Year,
+      y=:Count,
+      Geom.bar,
+      Scale.y_continuous(format=:plain)
+   )
+   Gadfly.push!(plt, Guide.title("Population"))
+   Gadfly.push!(plt, Guide.xlabel(nothing))
+   Gadfly.push!(plt, Guide.ylabel("# of citizens"))
+
+   normalized_plt = Gadfly.plot(
+      data,
+      x=:Year,
+      y=:Normalized,
+      Geom.bar,
+   )
+   Gadfly.push!(normalized_plt, Guide.title("Normalized"))
+   Gadfly.push!(normalized_plt, Guide.xlabel(nothing))
+   Gadfly.push!(normalized_plt, Guide.ylabel("% of citizens"))
+
    fig = Gadfly.hstack(plt, normalized_plt)
-   img = SVG("plot.svg")
+
+   img = SVG("plot.svg", 10inch, 6inch)
    draw(img, fig)
+
+   close(db)
 end
 
 plot(ARGS[1], ARGS[2], ARGS[3])

a5bb8b8ccad1ab303285550b55bd66a6c9fc45dc
jacksonet00 <jacksoneliastaylor@gmail.com>
2021-02-08 20:36:57 -0500

    refactored pipeline

--- prepare.jl
+++ prepare.jl
@@ -2,10 +2,7 @@ using ZipFile
 using SQLite
 using CSV
 
-rdr = ZipFile.Reader(ARGS[1])
-db = SQLite.DB(ARGS[2])
-
-drop = """
+drop_statement = """
 DROP TABLE IF EXISTS names;
 """
 
@@ -17,25 +14,35 @@ sex TEXT,
 num INTEGER
 );
 """
-insert = """
+insert_statement = """
 INSERT INTO names
 (year, name, sex, num)
 VALUES
 (?, ?, ?, ?);
 """
-insert_sql = DBInterface.prepare(db, insert)
 
-DBInterface.execute(db, drop)
-DBInterface.execute(db, schema)
+function load(zip_archive, db_name)
+   reader = ZipFile.Reader(zip_archive)
+   db = SQLite.DB(db_name)
 
-for f in rdr.files
-   if endswith(f.name, ".txt")
-      year = split(split(split(f.name, "/")[end], ".")[1], "yob")[end]
-      f_csv = CSV.File(f; header=false, footerskip=1)
-      println("Loading year $(year) from $(f.name)...")
-      for row in f_csv
-         DBInterface.execute(insert_sql, [year, row[1], row[2], row[3]])
+   DBInterface.execute(db, drop_statement)
+   DBInterface.execute(db, schema)
+
+   insert_sql = DBInterface.prepare(db, insert_statement)
+
+   for f in reader.files
+      if endswith(f.name, ".txt")
+         year = split(split(split(f.name, "/")[end], ".")[1], "yob")[end]
+         f_csv = CSV.File(f; header=false, footerskip=1)
+         println("Loading data from $(year)...")
+         for row in f_csv
+            DBInterface.execute(insert_sql, [year, row[1], row[2], row[3]])
+         end
       end
-      println("Completed :)")
-   end
+   end      
+
+   close(reader)
+   close(db)
 end
+
+load(ARGS[1], ARGS[2])
